151c5c1262b37aff54bdb975935cb057b62077d0
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-08 13:25:03 +0100

    feat: day1, add addcalories method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -17,12 +17,30 @@ public class UnitTest1
         
         Assert.Equal(0, adventDay1.GetMostCalories());
     }
+    
+    [Fact]
+    public void Add_Calories_To_An_Elf_returns_the_calories()
+    {
+        AdventDay1 adventDay1 = new();
+        adventDay1.AddCalories(100);
+        
+        Assert.Equal(100, adventDay1.GetMostCalories());
+    }
 }
 
 public class AdventDay1
 {
+    // 2
+    private int _totalCalories;
+
     public int GetMostCalories()
     {
-        return 0;
+        return _totalCalories; // 3
+    }
+
+    public void AddCalories(int calories)
+    {
+        // 1
+        _totalCalories = calories;
     }
 }
