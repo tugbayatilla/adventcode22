a4dd549f872b45f23462613c1d5eccb92fdc9fcd
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-08 14:30:59 +0100

    feat: day2, advent day 2 exist

---
+++ Day2/AdventDay2_UnitTests.cs
@@ -0,0 +1,19 @@
+namespace AdventOfCode22.Day2;
+
+public class AdventDay2UnitTests
+{
+    
+    [Fact]
+    public void _001_Day2_exist()
+    {
+        AdventDay2 adventDay2 = new();
+        
+        Assert.NotNull(adventDay2);
+    }
+    
+    
+}
+
+public class AdventDay2
+{
+}
