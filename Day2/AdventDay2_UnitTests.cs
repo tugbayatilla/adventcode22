ce1d58e35ee85379297e0df8b19e497382cc05bf
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-08 16:18:52 +0100

    refactor: day2, magic strings and merge tests

--- Day2/AdventDay2_UnitTests.cs
+++ Day2/AdventDay2_UnitTests.cs
@@ -13,6 +13,12 @@ public class AdventDay2UnitTests
     private const int PaperWinPoint = AdventDay2.PaperPoint + AdventDay2.WinPoint;
     private const int ScissorsWinPoint = AdventDay2.ScissorsPoint + AdventDay2.WinPoint;
     private const int RockWinPoint = AdventDay2.RockPoint + AdventDay2.WinPoint;
+    private const int PaperLosePoint = AdventDay2.PaperPoint;
+    private const int RockLosePoint = AdventDay2.RockPoint;
+    private const int ScissorsLosePoint = AdventDay2.ScissorsPoint;
+    private const int ScissorsDrawPoint = 6;
+    private const int PaperDrawPoint = 4;
+    private const int RockDrawPoint = 2;
 
     [Fact]
     public void _001_Day2_exist()
@@ -46,27 +52,31 @@ public class AdventDay2UnitTests
         Assert.Equal(1, adventDay2.Calculate(OppPaper, MeRock));
     }
 
-    [Fact]
+    [Fact(Skip = $"{nameof(_006_Situations)} is covering this test.")]
     public void _005_opponents_says_Scissors_and_i_say_scissors_then_we_draw_and_get_6_point()
     {
         AdventDay2 adventDay2 = new();
 
-        Assert.Equal(6, adventDay2.Calculate(OppScissors, MeScissors));
+        Assert.Equal(ScissorsDrawPoint, adventDay2.Calculate(OppScissors, MeScissors));
     }
 
     [Theory]
-    [InlineData(OppScissors, MeScissors, 6)]
-    [InlineData(OppPaper, MePaper, 4)]
-    [InlineData(OppRock, MeRock, 2)]
+    [InlineData(OppScissors, MeScissors, ScissorsDrawPoint)]
+    [InlineData(OppPaper, MePaper, PaperDrawPoint)]
+    [InlineData(OppRock, MeRock, RockDrawPoint)]
     [InlineData(OppRock, MePaper, PaperWinPoint)]
     [InlineData(OppPaper, MeScissors, ScissorsWinPoint)]
     [InlineData(OppScissors, MeRock, RockWinPoint)]
+    [InlineData(OppRock, MeScissors, ScissorsLosePoint)]
+    [InlineData(OppPaper, MeRock, RockLosePoint)]
+    [InlineData(OppScissors, MePaper, PaperLosePoint)]
     public void _006_Situations(string opp, string me, int expectedPoint)
     {
         AdventDay2 adventDay2 = new();
 
         Assert.Equal(expectedPoint, adventDay2.Calculate(opp, me));
     }
+
 }
 
 public class AdventDay2
