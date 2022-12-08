7b4abfcf0c6a99e5a1888b247cfae1e436f3b8ed
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-08 15:00:25 +0100

    test: day2, opp:scissors, me:scissors say then we draw and get 6 points

--- Day2/AdventDay2_UnitTests.cs
+++ Day2/AdventDay2_UnitTests.cs
@@ -26,6 +26,22 @@ public class AdventDay2UnitTests
         
         Assert.Equal(8, adventDay2.Calculate("A","Y"));
     }
+    
+    [Fact]
+    public void _004_opponents_says_Paper_and_i_say_Rock_then_i_lost_and_get_1_point()
+    {
+        AdventDay2 adventDay2 = new();
+        
+        Assert.Equal(1, adventDay2.Calculate("B","X"));
+    }
+    
+    [Fact]
+    public void _005_opponents_says_Scissors_and_i_say_scissors_then_we_draw_and_get_6_point()
+    {
+        AdventDay2 adventDay2 = new();
+        
+        Assert.Equal(6, adventDay2.Calculate("C","Z"));
+    }
 
 }
 
@@ -35,6 +51,13 @@ public class AdventDay2
     {
         if (opponentSelection == "A" && mySelection == "Y")
             return 8;
+        
+        if (opponentSelection == "B" && mySelection == "X")
+            return 1;
+        
+        if (opponentSelection == "C" && mySelection == "Z")
+            return 6;
+        
         
         return 0;
     }
