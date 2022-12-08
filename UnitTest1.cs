1bfcad7840c8b2221973ea3a636ecb7d28d77d0b
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-08 13:25:06 +0100

    test: day1, add multiple elves can add calories to the inventory

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -26,21 +26,57 @@ public class UnitTest1
         
         Assert.Equal(100, adventDay1.GetMostCalories());
     }
+    
+    [Fact]
+    public void Add_Calories_To_A_specific_Elf_returns_the_calories()
+    {
+        AdventDay1 adventDay1 = new();
+        adventDay1.AddCalories(100, 1);
+        
+        Assert.Equal(100, adventDay1.GetMostCalories());
+    }
+    
+    [Fact]
+    public void Add_Calories_for_2_Elves_returns_most_calories()
+    {
+        AdventDay1 adventDay1 = new();
+        adventDay1.AddCalories(100, 0);
+        adventDay1.AddCalories(200, 1);
+        
+        Assert.Equal(200, adventDay1.GetMostCalories());
+    }
+    
+    [Fact]
+    public void Add_Calories_for_2_Elves_multiple_calories_returns_most_calories()
+    {
+        AdventDay1 adventDay1 = new();
+        adventDay1.AddCalories(100, 0);
+        adventDay1.AddCalories(200, 0);
+        
+        adventDay1.AddCalories(200, 1);
+        adventDay1.AddCalories(300, 1);
+
+        Assert.Equal(500, adventDay1.GetMostCalories());
+    }
 }
 
 public class AdventDay1
 {
-    // 2
-    private int _totalCalories;
+    private readonly Dictionary<int, List<int>> _inventory = new();
 
     public int GetMostCalories()
     {
-        return _totalCalories; // 3
+        if (_inventory.Count == 0) return 0;
+        
+        return _inventory.Values.Select(p=>p.Sum()).Max();
     }
 
-    public void AddCalories(int calories)
+    public void AddCalories(int calories, int indexOfElf = 0)
     {
-        // 1
-        _totalCalories = calories;
+        if (!_inventory.ContainsKey(indexOfElf))
+        {
+            _inventory.Add(indexOfElf, new List<int>());
+        }
+        _inventory[indexOfElf].Add(calories);
     }
 }
