707e36c1551aa6baf5e6007e55e64dc15679fa45
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-09 13:26:27 +0100

    refactor: day3, extract fields

--- Day3/AdventDay3.cs
+++ Day3/AdventDay3.cs
@@ -3,6 +3,8 @@ namespace AdventOfCode22.Day3;
 public class AdventDay3
 {
     private int _sumOfPriorityItems;
+    private readonly string _upperLetterList = string.Join("",Enumerable.Range('A', 'Z' - 'A' + 1).Select(c => (char)c));
+    private readonly string _lowerLetterList = string.Join("",Enumerable.Range('a', 'z' - 'a' + 1).Select(c => (char)c));
 
     public int SumOfPriorities()
     {
@@ -11,10 +13,7 @@ public class AdventDay3
 
     public void IdentifyAndStorePriorityItem(string priorityItem)
     {
-        var upperLetterList = string.Join("",Enumerable.Range('A', 'Z' - 'A' + 1).Select(c => (char)c));
-        var lowerLetterList = string.Join("",Enumerable.Range('a', 'z' - 'a' + 1).Select(c => (char)c));
-
-        var fullTable = string.Concat(lowerLetterList, upperLetterList);
+        var fullTable = string.Concat(_lowerLetterList, _upperLetterList);
             
         _sumOfPriorityItems += fullTable.IndexOf(priorityItem) + 1;
     }
