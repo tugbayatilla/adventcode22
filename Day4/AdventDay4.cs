6965c24d401e57c00413fa225c102263f422b060
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-09 16:55:52 +0100

    feat: day4, add pair definition

--- Day4/AdventDay4.cs
+++ Day4/AdventDay4.cs
@@ -2,8 +2,15 @@ namespace AdventOfCode22.Day4;
 
 public class AdventDay4
 {
+    private int _totalPairs;
+
     public int GetNumberOfPairsFullyCoverOther()
     {
-        return 0;
+        return _totalPairs;
+    }
+
+    public void AddPairDefinition(string pairDefinition)
+    {
+        _totalPairs = 1;
     }
 }
--- Day4/AdventDay4_UnitTests.cs
+++ Day4/AdventDay4_UnitTests.cs
@@ -8,4 +8,13 @@ public class AdventDay4UnitTests
         AdventDay4 adventDay4 = new();
         Assert.Equal(0, adventDay4.GetNumberOfPairsFullyCoverOther());
     }
+    
+    [Fact]
+    public void _002_one_pair_has_same_assignment_returns_1()
+    {
+        AdventDay4 adventDay4 = new();
+        adventDay4.AddPairDefinition("1-1,1-1");
+        
+        Assert.Equal(1, adventDay4.GetNumberOfPairsFullyCoverOther());
+    }
 }
