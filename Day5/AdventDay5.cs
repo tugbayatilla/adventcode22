e644be19267311e2e55e033a2e0dc708221aa712
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-22 11:19:22 +0100

    refactor: make crateName optional and remove single parameter AddCrateToStack method

--- Day5/AdventDay5.cs
+++ Day5/AdventDay5.cs
@@ -2,19 +2,14 @@ namespace AdventOfCode22.Day5;
 
 public class AdventDay5
 {
-    private readonly List<List<string>> _cargo = new(){new()};
+    private readonly List<List<string>> _cargo = new() {new()};
 
     public IEnumerable<IEnumerable<string>> GetCargoState()
     {
         return _cargo;
     }
-
-    public void AddCrateToStack(int stackId)
-    {
-        _cargo.Skip(stackId - 1).First().Add("");
-    }
-
-    public void AddCrateToStack(int stackId, string crateName)
+    
+    public void AddCrateToStack(int stackId, string crateName = "")
     {
         _cargo.Skip(stackId - 1).First().Add(crateName);
     }
