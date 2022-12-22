9d7e155cb954b912a605050098e8d346dbf1118e
Tugbay Atilla <tugbayatilla@gmail.com>
2022-12-22 11:14:05 +0100

    refactor: extract field for AdventDay5 class and reformat code

--- Day5/AdventDay5_UnitTests.cs
+++ Day5/AdventDay5_UnitTests.cs
@@ -2,37 +2,34 @@ namespace AdventOfCode22.Day5;
 
 public class AdventDay5UnitTests
 {
-    
+    private readonly AdventDay5 _adventDay5 = new();
+
     [Fact]
     public void _001_get_cargo_is_null()
     {
-        AdventDay5 adventDay5 = new();
-        Assert.NotNull(adventDay5.GetCargoState());
+        Assert.NotNull(_adventDay5.GetCargoState());
     }
-    
+
     [Fact]
     public void _002_cargo_has_one_emtpy_stack()
     {
-        AdventDay5 adventDay5 = new();
-        Assert.Equal(1,adventDay5.GetCargoState().Count());
+        Assert.Equal(1, _adventDay5.GetCargoState().Count());
     }
-    
+
     [Fact]
     public void _003_cargo_has_one_crate_in_one_stack()
     {
-        AdventDay5 adventDay5 = new();
-        adventDay5.AddCrateToStack(1);
-        var firstStack = adventDay5.GetCargoState().First();
-        Assert.Equal(1,firstStack.Count());
+        _adventDay5.AddCrateToStack(1);
+        var firstStack = _adventDay5.GetCargoState().First();
+        Assert.Equal(1, firstStack.Count());
     }
-    
+
     [Fact]
     public void _004_cargo_has_two_crates_in_one_stack()
     {
-        AdventDay5 adventDay5 = new();
-        adventDay5.AddCrateToStack(1);
-        adventDay5.AddCrateToStack(1);
-        var firstStack = adventDay5.GetCargoState().First();
-        Assert.Equal(2,firstStack.Count());
+        _adventDay5.AddCrateToStack(1);
+        _adventDay5.AddCrateToStack(1);
+        var firstStack = _adventDay5.GetCargoState().First();
+        Assert.Equal(2, firstStack.Count());
     }
 }
