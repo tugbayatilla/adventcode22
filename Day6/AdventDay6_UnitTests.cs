9f8c49fdf14969f9a4ece33e5e87069fb3d3ccf1
Tugbay Atilla <tugbayatilla@gmail.com>
2023-01-06 13:47:43 +0100

    tests: the theory covers now marker is zero when buffer is empty

--- Day6/AdventDay6_UnitTests.cs
+++ Day6/AdventDay6_UnitTests.cs
@@ -3,12 +3,6 @@ namespace AdventOfCode22.Day6;
 public class AdventDay6UnitTests
 {
     [Fact]
-    public void Marker_is_zero_when_buffer_is_empty()
-    {
-        Assert.Equal(0,ElfCommSystem.FindMarker(""));
-    }
-    
-    [Fact]
     public void Marker_is_zero_when_buffer_contains_less_than_four_chars()
     {
         Assert.Equal(0,ElfCommSystem.FindMarker("asd"));
@@ -18,6 +12,13 @@ public class AdventDay6UnitTests
     public void Marker_is_four_when_buffer_only_contains_four_different_chars()
     {
         Assert.Equal(4,ElfCommSystem.FindMarker("abcd"));
+    }
+    
+    [Theory]
+    [InlineData("", 0)]
+    public void Marker_finds_when_buffer_given(string buffer, int marker)
+    {
+        Assert.Equal(marker,ElfCommSystem.FindMarker(buffer));
     }
     
     
