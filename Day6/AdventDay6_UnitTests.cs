4501f930f95704396fc83fdcb5e165f7b35a1513
Tugbay Atilla <tugbayatilla@gmail.com>
2023-01-06 13:50:23 +0100

    test: marker is 5 when buffer has five chars and first two are the same

--- Day6/AdventDay6_UnitTests.cs
+++ Day6/AdventDay6_UnitTests.cs
@@ -6,6 +6,7 @@ public class AdventDay6UnitTests
     [InlineData("", 0)]
     [InlineData("asd", 0)]
     [InlineData("abcd", 4)]
+    [InlineData("aacde", 5)]
     public void Marker_finds_when_buffer_given(string buffer, int marker)
     {
         Assert.Equal(marker,ElfCommSystem.FindMarker(buffer));
