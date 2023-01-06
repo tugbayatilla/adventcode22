3a97126f892e7abe44737be8d13f8cb2d3c193bc
Tugbay Atilla <tugbayatilla@gmail.com>
2023-01-06 13:48:48 +0100

    tests: all merged into a theory

--- Day6/AdventDay6_UnitTests.cs
+++ Day6/AdventDay6_UnitTests.cs
@@ -2,20 +2,10 @@ namespace AdventOfCode22.Day6;
 
 public class AdventDay6UnitTests
 {
-    [Fact]
-    public void Marker_is_zero_when_buffer_contains_less_than_four_chars()
-    {
-        Assert.Equal(0,ElfCommSystem.FindMarker("asd"));
-    }
-    
-    [Fact]
-    public void Marker_is_four_when_buffer_only_contains_four_different_chars()
-    {
-        Assert.Equal(4,ElfCommSystem.FindMarker("abcd"));
-    }
-    
     [Theory]
     [InlineData("", 0)]
+    [InlineData("asd", 0)]
+    [InlineData("abcd", 4)]
     public void Marker_finds_when_buffer_given(string buffer, int marker)
     {
         Assert.Equal(marker,ElfCommSystem.FindMarker(buffer));
