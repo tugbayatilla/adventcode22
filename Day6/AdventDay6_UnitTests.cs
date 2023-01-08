21973801685e24307570cd758fe3c3ce9b25c1c0
Tugbay Atilla <tugbayatilla@gmail.com>
2023-01-08 11:38:57 +0100

    test: sample test data is not in theory

--- Day6/AdventDay6_UnitTests.cs
+++ Day6/AdventDay6_UnitTests.cs
@@ -8,6 +8,10 @@ public class AdventDay6UnitTests
     [InlineData("abcd", 4)]
     [InlineData("__cde", 5)]
     [InlineData("____", 0)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
     public void Marker_finds_when_buffer_given(string buffer, int marker)
     {
         Assert.Equal(marker,ElfCommSystem.FindMarker(buffer));
